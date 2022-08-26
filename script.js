9de298bc2060223b09e912510bc086d45641fff6
Aziz <aziz.cse.cu@gmail.com>
2022-08-27 01:19:42 +0600

    added files

---
+++ script.js
@@ -0,0 +1,24 @@
+setInterval(setClock, 1000);
+
+const secondHand = document.querySelector('[data-second-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const hourHand = document.querySelector('[data-hour-hand]');
+
+function setClock() {
+    const time = new Date();
+    const second = time.getSeconds() / 60;
+    const minute = (second + time.getMinutes())/60;
+    const hour = (minute + time.getHours())/12;
+    console.log(second);
+    console.log(minute);
+    console.log(hour);
+    
+
+    setRotation(secondHand, second);
+    setRotation(minuteHand, minute);
+    setRotation(hourHand, hour);
+}
+
+function setRotation (element, rotationRation) {
+    element.style.setProperty('--rotation', rotationRation*360);
+}
